7324b0c21a94cdf2f6a1d33130d18e5c8c34ac64
Ali Mostafa <amostafa1@cps.edu>
2025-05-01 11:30:21 -0500

    upd

--- main.py
+++ main.py
@@ -4,18 +4,23 @@ from sklearn.preprocessing import MinMaxScaler, LabelEncoder
 from neural import *
 
 data_path = 'dry_bean_dataset.csv'
-data = pd.read_csv(data_path)
+dataframe = pd.read_csv(data_path)
 
-x = data.drop('Class', axis=1)
-y = data['Class']
+train_columns = ['Area', 'Perimeter', 'Roundness']
+
+x = dataframe[train_columns]
+y = dataframe['Class']
 
 scaler = MinMaxScaler()
 
 x_scaled = scaler.fit_transform(x)
 x_scaled_data = pd.DataFrame(x_scaled, columns=x.columns)
+print(x_scaled_data)
 
 encoder = LabelEncoder()
 y_encoded = encoder.fit_transform(y)
+
+print(y_encoded)
 
 training_data = []
 for i in range(len(x_scaled)):
@@ -23,13 +28,12 @@ for i in range(len(x_scaled)):
     training_data.append(sample)
 
 training_data_len = len(training_data)
-training_size = 0.8
+training_size = 0.1
 split_index = int(training_data_len * training_size)
-# training, testing = np.split(training_data, [int(training_data_len * training_size)])
 
 training = training_data[:split_index]
 testing = training_data[split_index:]
 
-bean_network = NeuralNet(16, 16, 1)
+bean_network = NeuralNet(3, 12, 1)
 bean_network.train(training, print_interval=1)
 print(bean_network.test_with_expected(testing))
