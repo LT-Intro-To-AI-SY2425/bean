afb53bbda19f62572f4891a58d7ba78faba5a63d
Ali Mostafa <amostafa1@cps.edu>
2025-05-02 11:27:37 -0500

    upd!

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import numpy as np
+import random
 from sklearn.preprocessing import MinMaxScaler, LabelEncoder
 from neural import *
 
@@ -15,27 +15,31 @@ scaler = MinMaxScaler()
 
 x_scaled = scaler.fit_transform(x)
 x_scaled_data = pd.DataFrame(x_scaled, columns=x.columns)
-print(x_scaled_data)
 
 encoder = LabelEncoder()
 y_encoded = encoder.fit_transform(y)
+for i, v in enumerate(y_encoded):
+    y_encoded[i] = v/7
 
-print(y_encoded)
-
-training_data = []
+testing_data = []
 for i in range(len(x_scaled)):
     sample = (x_scaled[i].tolist(), [y_encoded[i]])
-    training_data.append(sample)
+    testing_data.append(sample)
 
 
-training_data_len = len(training_data)
-training_size = 0.01
-split_index = int(training_data_len * training_size)
+testing_data_len = len(testing_data)
+training_size = 0.2
+split_index = int(testing_data_len * training_size)
 
-training = training_data[:split_index]
-testing = training_data[split_index:]
+training_data = []
 
-print(training)
+for i in range(0, split_index):
+    training_data.append(testing_data.pop(random.randint(0, len(testing_data) - 1)))
+
 bean_network = NeuralNet(3, 12, 1)
-bean_network.train(training, print_interval=1)
-print(bean_network.test_with_expected(testing))
+bean_network.train(training_data, iters=30, print_interval=1)
+result = bean_network.test_with_expected(testing_data)
+
+for x in result:
+    print(f"actual: {x[1]}")
+    print(f"predicted: {x[2]}")
